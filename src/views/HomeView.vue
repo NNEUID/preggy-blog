e7580634ba2eaf6afb151ef5ca16eae778932bdb
NNEUID <nneuid@gmail.com>
2023-02-04 01:11:35 +0000

    props and computed properties in the composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Home</h1>
+  <PostList :posts="posts" />
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from '../components/PostList.vue'
+import { ref } from '@vue/reactivity';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    PostList
+  },
+  setup() {
+
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo quia architecto iste, nemo ipsam molestiae odio repudiandae doloribus cum voluptas culpa illum exercitationem harum! Perferendis a veritatis dolores dolorum earum nulla nesciunt repudiandae placeat voluptas quisquam, dolorem sunt aliquam in voluptatem harum obcaecati fugit, deserunt quae consequuntur quia. Consectetur saepe rerum, qui fugit repudiandae sunt distinctio labore tempora accusantium assumenda nesciunt reprehenderit ipsa vel fugiat dicta quidem maxime ipsum ea? Veniam, saepe odit facere, qui nam tempore perferendis quam obcaecati eveniet, distinctio dolorum incidunt optio corrupti tenetur neque quibusdam aliquid! Nihil illum corporis unde corrupti nam assumenda facilis. Perspiciatis, assumenda.', id: 1 },
+      { title: 'top personalised antenatal care just for you', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo quia architecto iste, nemo ipsam molestiae odio repudiandae doloribus cum voluptas culpa illum exercitationem harum! Perferendis a veritatis dolores dolorum earum nulla nesciunt repudiandae placeat voluptas quisquam, dolorem sunt aliquam in voluptatem harum obcaecati fugit, deserunt quae consequuntur quia. Consectetur saepe rerum, qui fugit repudiandae sunt distinctio labore tempora accusantium assumenda nesciunt reprehenderit ipsa vel fugiat dicta quidem maxime ipsum ea? Veniam, saepe odit facere, qui nam tempore perferendis quam obcaecati eveniet, distinctio dolorum incidunt optio corrupti tenetur neque quibusdam aliquid! Nihil illum corporis unde corrupti nam assumenda facilis. Perspiciatis, assumenda.', id: 2 },
+    ])
+
+    return { posts }
   }
 }
 </script>
