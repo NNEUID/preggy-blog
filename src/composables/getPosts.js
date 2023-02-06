bb81349d1403f96ff14c21083c2697b316d4ebe1
NNEUID <nneuid@gmail.com>
2023-02-06 22:49:06 +0000

    add a reusable loading spinner component

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -6,7 +6,10 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      let data = await fetch('http://localhost:3000/posts')
+      await new Promise(resolve => {
+        setTimeout(resolve, 2000)
+      })
+      const data = await fetch('http://localhost:3000/posts')
       if (!data.ok) {
         throw Error('No data available')
       }
