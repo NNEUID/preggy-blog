1bbfefa2ada52470d153f50ce6ca3be082955157
NNEUID <nneuid@gmail.com>
2023-02-06 23:18:48 +0000

    add form and state with setup + add functionality to add tags when Enter key is pressed

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,10 +1,44 @@
 <template>
-  Create Blog
+  <div class="create">
+    <form>
+
+      <label for="title">Title:</label>
+      <input v-model="title" name="title" type="text" required>
+
+      <label for="content">Content:</label>
+      <textarea v-model="body" name="content" id="content" cols="30" rows="10" required></textarea>
+
+      <label for="tags">Tags (hit enter to add a tag)</label>
+      <input v-model="tag" name="tags" type="text" @keydown.enter.prevent="handleKeyDown">
+
+      <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
+
+
+      <button>Add Post</button>
+
+    </form>
+  </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 export default {
+  setup() {
+    const title = ref('')
+    const body = ref('')
+    const tag = ref('')
+    const tags = ref(['html'])
 
+    const handleKeyDown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/, '') //removes all whitespace
+        tags.value.push(tag.value)
+      }
+      tag.value = ''
+    }
+
+    return { title, body, tag, handleKeyDown, tags }
+  }
 }
 </script>
 
