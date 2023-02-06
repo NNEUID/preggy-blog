3474a156a2b1c0a184501e3215ecb7a3fa696582
NNEUID <nneuid@gmail.com>
2023-02-06 22:11:43 +0000

    accept post id prop id in details page + write a getPost composable to get a single post by id

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,8 +3,17 @@
 </template>
 
 <script>
+import getPost from '@/composables/getPost';
 export default {
+  props: ['id'],
+  setup(props) {
+    const { post, error, load } = getPost(props.id)
+    load()
 
+    console.log(post)
+
+    return { post, error }
+  }
 }
 </script>
 
