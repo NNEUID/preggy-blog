10ddaeafe1de8d385b2eb0ddc34aa948a7bf6adf
NNEUID <nneuid@gmail.com>
2023-02-06 22:18:20 +0000

    add css styles to post details

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -20,5 +20,22 @@ export default {
 </script>
 
 <style>
+.tags a {
+  margin-right: 10px;
+}
 
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+
+.pre {
+  white-space: pre-wrap;
+}
 </style>
