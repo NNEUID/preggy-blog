eceff83a52284091bc67873947dddcad3b01b634
NNEUID <nneuid@gmail.com>
2023-02-07 00:43:11 +0000

    create Navbar component and routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>The Preggy Blog</h1>
+    <nav>
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link :to="{ name: 'create' }">Create Post</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
