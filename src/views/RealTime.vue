7635b7496313e6640b6a49affe46a9e83bb4accc
NNEUID <nneuid@gmail.com>
2023-02-08 12:34:22 +0000

    add onSnapshot to listen for real-time changes

--- src/views/RealTime.vue
+++ src/views/RealTime.vue
@@ -1,10 +1,27 @@
 <template>
-  <h3>Real-time Data</h3>
+  <h1>Real-time Data</h1>
+  <p v-if="posts.length">{{ posts[0].title }}</p>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import { db } from '@/firebase/config';
+import { onSnapshot, query, collection, orderBy } from '@firebase/firestore';
 export default {
+  setup() {
+    const posts = ref([])
 
+    const postsRef = collection(db, 'posts')
+    const postsQuery = query(postsRef, orderBy('createdAt', 'desc'))
+    const unsubscribe = onSnapshot(postsQuery, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        posts.value.push({ id: doc.id, ...doc.data() })
+      });
+    })
+
+
+    return { posts }
+  }
 }
 </script>
 
